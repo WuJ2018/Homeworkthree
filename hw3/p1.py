06804c89ffb31331db796781bdcd5f6530bd3c2f
WuJ2018 <cd>
2018-02-12 09:46:23 -0600

    homeworkthree

---
+++ hw3/p1.py
@@ -0,0 +1,22 @@
+#integer^
+number1 = 4
+number2 = 8
+number3 = number1 + number2
+print(number3)
+
+#floating point
+number1 = 4.0
+number2 = 8.0
+number3 = number1 / number2
+print(number3)
+
+#string
+greeting1 = raw_input("What is your first name?"
+greeting2 = raw_input("What is your last name?"
+print("yes this works," + greeting1 +" "+ greeting2)
+
+#boolean
+ItsRainingOutside = True
+CadetsAreOutSide = True
+CadetsNeedUmbrella = ItsRainingOutsie + CdetsAreOutSide
+print(CadetNeedUmbrella)
